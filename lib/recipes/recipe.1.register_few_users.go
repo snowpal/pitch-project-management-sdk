e0e3eb204feafe22c47bbf753b27e1b15ff9d1b7
AnishGhumara <khatri.anis@gmail.com>
2023-06-07 00:40:12 +0530

    fix: added config support

--- lib/recipes/recipe.1.register_few_users.go
+++ lib/recipes/recipe.1.register_few_users.go
@@ -8,28 +8,28 @@ import (
 
 func RegisterFewUsers() {
 	log.Info("Objective: Sign up a few new users")
-	_, err := recipes.RegisterUser(lib.DefaultEmail)
+	// _, err := recipes.RegisterUser(lib.DefaultEmail)
+	// if err != nil {
+	// 	return
+	// }
+
+	_, err := recipes.RegisterUser(lib.ActiveUser)
 	if err != nil {
 		return
 	}
 
-	_, err = recipes.RegisterUser(lib.ActiveUser)
-	if err != nil {
-		return
-	}
+	// _, err = recipes.RegisterUser(lib.AdminUser)
+	// if err != nil {
+	// 	return
+	// }
 
-	_, err = recipes.RegisterUser(lib.AdminUser)
-	if err != nil {
-		return
-	}
+	// _, err = recipes.RegisterUser(lib.ReadUser)
+	// if err != nil {
+	// 	return
+	// }
 
-	_, err = recipes.RegisterUser(lib.ReadUser)
-	if err != nil {
-		return
-	}
-
-	_, err = recipes.RegisterUser(lib.WriteUser)
-	if err != nil {
-		return
-	}
+	// _, err = recipes.RegisterUser(lib.WriteUser)
+	// if err != nil {
+	// 	return
+	// }
 }
--- lib/user.constants.go
+++ lib/user.constants.go
@@ -4,7 +4,7 @@ const Password = "Welcome1!"
 
 const (
 	DefaultEmail = "api_default@gmail.com"
-	ActiveUser   = "api_active_user@gmail.com"
+	ActiveUser   = "prj_mgmt_api_active_user@gmail.com"
 	ReadUser     = "api_read_user@gmail.com"
 	WriteUser    = "api_write_user@yopmail.com"
 	AdminUser    = "api_admin_user@gmail.com"
